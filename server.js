38f6890f36a31e86aa161db6a52a97b0e204853f
Nuthan S S <nss@icims.com>
2023-05-03 21:24:02 +0530

    Added contact.jg and html

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public', 'index.html'));
-});
+// app.use('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public', 'index.html'));
+// });
 
 // app.use('/contact', (req, res) => {
 //     res.sendFile(path.join(__dirname, './public', 'contact.html'));
 // });
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000...');
+const PORT = 3002;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
 });
